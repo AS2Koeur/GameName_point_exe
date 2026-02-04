ead2590989e5def6e53d8d5210913af84509f3f8
AS2Koeur <cozma.alexandrina@gmail.com>
2026-02-04 15:21:50 +0100

    chgt script et gitignore
    changement dans le script de scene 2 + ajout de choses à ignorer dans gitignore

--- scenes/scene_2_chambre_manager.gd
+++ scenes/scene_2_chambre_manager.gd
@@ -1,24 +1,12 @@
 extends Control
 
 
-@onready var textBox = $textBox
-#@onready var textBox = $textBox
 
-@export var dialogue : Array[String]
-#@export var dialogue : Array[String]
-
-var DialogueIndex = 0
 #var DialogueIndex = 0
 func _ready() -> void:
-	await get_tree().create_timer(5.0).timeout
-	await get_tree().create_timer(2.0)
+	
+	await get_tree().create_timer(2.0).timeout
 	$BegininTimer.start()
-<<<<<<< HEAD
-	print("Release me")
-	print("Release me")
-	print("gay'nt")
-	print("JENAIMARRE")
-=======
 	await $AudioStreamPlayer2D.finished
 	if Dialogic.current_timeline != null:
 		return
@@ -26,33 +14,8 @@ func _ready() -> void:
 	Dialogic.start("res://dialogTimeline/Scene2.dtl")
 	get_viewport().set_input_as_handled()
 		
->>>>>>> Alex
+
 func _on_timer_timeout() -> void:
 	$AudioStreamPlayer2D.play()
-	await get_tree().create_timer(2.0).timeout
-	textBox.visible = true
-	await get_tree().create_timer(2.0).timeout
-	$textBox/MarginContainer/Label.visible = true
-	$dialogTimer.start()
-	#await get_tree().create_timer(2.0).timeout
-	#textBox.visible = true
-	#await get_tree().create_timer(2.0).timeout
-	#$textBox/MarginContainer/Label.visible = true
-	#$dialogTimer.start()
 	
-
-
-func _on_dialog_timer_timeout() -> void:
-	if DialogueIndex < 3:
-		$textBox/MarginContainer/Label.text = dialogue[DialogueIndex]
-		DialogueIndex += 1
-		print(DialogueIndex)
-	else:
-		$dialogTimer.one_shot = true
-	pass
-	#if DialogueIndex < 3:
-		#$textBox/MarginContainer/Label.text = dialogue[DialogueIndex]
-		#DialogueIndex += 1
-		#print(DialogueIndex)
-	#else:
-		#$dialogTimer.one_shot = true
+	
