2b0c74edc702493c33f0c74d406ea4b305165650
AS2Koeur <cozma.alexandrina@gmail.com>
2026-02-04 17:12:46 +0100

    push scene chambre almost done

--- scenes/scene_2_chambre_manager.gd
+++ scenes/scene_2_chambre_manager.gd
@@ -1,21 +1,37 @@
 extends Control
 
 
-
-#var DialogueIndex = 0
+@onready var reveil = $AnimationPlayer
+@onready var poster = $AnimatedSprite2D
+var posterIndex = 1
 func _ready() -> void:
 	
 	await get_tree().create_timer(2.0).timeout
 	$BegininTimer.start()
+	
 	await $AudioStreamPlayer2D.finished
 	if Dialogic.current_timeline != null:
 		return
 	await get_tree().create_timer(2.0)
 	Dialogic.start("res://dialogTimeline/Scene2.dtl")
 	get_viewport().set_input_as_handled()
-		
+	
+func _process(float) -> void:
+	_poster_change()
 
 func _on_timer_timeout() -> void:
 	$AudioStreamPlayer2D.play()
+	reveil.play("reveil")
+	await $AudioStreamPlayer2D.finished
+	reveil.stop()
 	
-	
+func _poster_change():
+	$AnimatedSprite2D.play(str(posterIndex))
+	if posterIndex <= 5 && Input.is_action_just_pressed("SkipChambre"):
+		posterIndex += 1
+		print(posterIndex)
+	elif posterIndex > 5 && Input.is_action_just_pressed("SkipChambre"):
+		get_tree().change_scene_to_file("res://scenes/wolaonarrivealecole.tscn") 
+
+func _on_skip_pressed() -> void:
+	get_tree().reload_current_scene()
