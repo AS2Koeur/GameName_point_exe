a770e60afa1b23a764f7e243ef1e6de911457e47
AS2Koeur <cozma.alexandrina@gmail.com>
2026-02-05 11:38:40 +0100

    salle info

--- scripts/salle_info.gd
+++ scripts/salle_info.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Dialogic.start("res://dialogTimeline/Scene5.dtl")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +13,4 @@ func _process(delta: float) -> void:
 
 func _on_sortie_body_entered(body: CharacterBody2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/Scene4Hall.tscn")
+	Dialogic.end_timeline(true)
