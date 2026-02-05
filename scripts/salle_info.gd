7c011a587fbd9af840aa860faaf4174753f39cde
AS2Koeur <cozma.alexandrina@gmail.com>
2026-02-05 12:30:48 +0100

    Salle info presque finie

--- scripts/salle_info.gd
+++ scripts/salle_info.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+@onready var discussionDiscord = $ScreenDiscord
+var ScreenDespawn = false
 func _ready() -> void:
 	Dialogic.start("res://dialogTimeline/Scene5.dtl")
 
@@ -14,3 +15,31 @@ func _process(delta: float) -> void:
 func _on_sortie_body_entered(body: CharacterBody2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/Scene4Hall.tscn")
 	Dialogic.end_timeline(true)
+
+
+func _on_first_pc_body_entered(body: CharacterBody2D) -> void:
+	Dialogic.start("res://dialogTimeline/Scene5.dtl", "interaction PC1")
+
+
+func _on_second_pc_body_entered(body: CharacterBody2D) -> void:
+	Dialogic.start("res://dialogTimeline/Scene5.dtl", "interaction PC3")
+
+
+func _on_third_pc_body_entered(body: CharacterBody2D):
+	Dialogic.start("res://dialogTimeline/Scene5.dtl", "interaction LEPC")
+	await get_tree().create_timer(4).timeout
+	discussionDiscord.visible = true
+	await get_tree().create_timer(2).timeout
+	discussionDiscord.visible = false
+	ScreenDespawn = true
+	if ScreenDespawn:
+		Dialogic.start("res://dialogTimeline/Scene5.dtl", "CrashOut")
+	
+	
+	
+	
+	
+
+
+func _on_fourth_pc_body_entered(body: CharacterBody2D) -> void:
+	Dialogic.start("res://dialogTimeline/Scene5.dtl", "interaction PC2")
